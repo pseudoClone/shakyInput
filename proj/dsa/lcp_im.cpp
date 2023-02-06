2595a6bec2a8b25d161d6414425e65f24ed4e7af
pseudoclone <prince.077bei027@acem.edu.np>
2023-02-06 20:44:20 +0545

    Folder addition for all projects done. Right now for DSA!

---
+++ proj/dsa/lcp_im.cpp
@@ -0,0 +1,80 @@
+#include <unordered_map>
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <cstdbool>
+
+using namespace std;
+
+struct trie_word
+{
+	bool wordState;
+	unordered_map<char, trie_word*> character;
+
+	trie_word() {
+		wordState = 0;
+	}
+};
+
+void insert(trie_word* root, string str)
+{
+
+	trie_word* temp = root;
+ 
+	for (char char_val: str)
+	{
+		
+		if (temp -> character . find(char_val) == temp -> character . end()) {
+			temp -> character[char_val] = new trie_word();
+		}
+ 
+		temp = temp -> character[char_val];
+	}
+ 
+	temp -> wordState = true;
+}
+ 
+
+string findLCP(vector<string> dict)
+{
+
+	trie_word* head = new trie_word();
+	
+	for (string s: dict) {
+		insert(head, s);
+	}
+
+	string lcp;
+	
+	trie_word* temp = head;
+ 
+	// Loop only if wordState = true || node has > 1 child
+	while (temp && !temp -> wordState && (temp -> character.size() == 1))
+	{
+		// Assign iterator unordered_map
+		auto i = temp -> character.begin();
+ 
+
+		lcp += i -> first; // Add to current charVal
+ 
+
+		temp = i -> second;
+	}
+ 
+	return lcp;
+}
+ 
+int main(int argc, char **argv)
+{
+	vector<string> word_l;	// Keys
+	string str_Val;
+	
+	for(size_t i = 0; i < 5; ++i)
+	{
+		cout<<"Enter the string: "<<i+1<<": ";
+		cin>> str_Val;
+		word_l.push_back(str_Val);
+	}
+	cout <<std::endl<<"LCP: " << findLCP(word_l)<<"\n";
+	return 0;
+}
